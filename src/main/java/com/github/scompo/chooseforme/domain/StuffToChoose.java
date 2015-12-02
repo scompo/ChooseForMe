45ff34c171b90d33903a748bbf5218bfbe6ff689
Mauro Scomparin <scompo@gmail.com>
2015-12-02 22:37:27 +0100

    added StuffToChoose

---
+++ src/main/java/com/github/scompo/chooseforme/domain/StuffToChoose.java
@@ -0,0 +1,19 @@
+package com.github.scompo.chooseforme.domain;
+
+public class StuffToChoose {
+
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "StuffToChoose [name=" + name + "]";
+	}
+}
