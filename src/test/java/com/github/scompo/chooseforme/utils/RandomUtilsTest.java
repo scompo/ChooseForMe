3109cfd70b0b0592dca5c3b64b4b693df38e5863
Mauro Scomparin <scompo@gmail.com>
2015-12-03 18:53:15 +0100

    Added RandomUtilsTest stub

---
+++ src/test/java/com/github/scompo/chooseforme/utils/RandomUtilsTest.java
@@ -0,0 +1,14 @@
+package com.github.scompo.chooseforme.utils;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class RandomUtilsTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
