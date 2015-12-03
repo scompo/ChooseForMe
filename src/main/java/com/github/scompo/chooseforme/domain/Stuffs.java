efbffff09f204f8dffe223795fc91730eb6d5dcc
Mauro Scomparin <scompo@gmail.com>
2015-12-03 01:33:23 +0100

    comments and serialversionuid
    removed comments from default constructors and moved serialversionuid to
    top of Stuffs

--- src/main/java/com/github/scompo/chooseforme/domain/Stuffs.java
+++ src/main/java/com/github/scompo/chooseforme/domain/Stuffs.java
@@ -4,23 +4,20 @@ import java.util.ArrayList;
 import java.util.Collection;
 
 public class Stuffs extends ArrayList<StuffToChoose>{
+	
+	private static final long serialVersionUID = 3401471504840787452L;
 
 	public Stuffs() {
 		super();
-		// TODO Auto-generated constructor stub
 	}
 
 	public Stuffs(Collection<? extends StuffToChoose> c) {
 		super(c);
-		// TODO Auto-generated constructor stub
 	}
 
 	public Stuffs(int initialCapacity) {
 		super(initialCapacity);
-		// TODO Auto-generated constructor stub
 	}
-
-	private static final long serialVersionUID = 3401471504840787452L;
 
 	@Override
 	public String toString() {
