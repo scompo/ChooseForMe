3ff2edc3ef032a923cab379e47a5eb02850a43a3
Mauro Scomparin <scompo@gmail.com>
2015-12-03 18:53:37 +0100

    added RandomServiceTest

---
+++ src/test/java/com/github/scompo/chooseforme/services/RandomServiceTest.java
@@ -0,0 +1,14 @@
+package com.github.scompo.chooseforme.services;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class RandomServiceTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
